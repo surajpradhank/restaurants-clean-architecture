aacb38eafd8966963e6b751773f990af76d51fb1
surajpradhank <surajkumarpradhan2016@gmail.com>
2024-10-13 19:36:28 +0530

    Add project files.

---
+++ Restarurants.API/Program.cs
@@ -0,0 +1,26 @@
+using Restaurants.Infrastructure.Extension;
+using Restaurants.Infrastructure.Seeders;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+
+builder.Services.AddInfrastructure(builder.Configuration);
+
+var app = builder.Build();
+
+var scope = app.Services.CreateScope();
+var seeder = scope.ServiceProvider.GetRequiredService<IRestaurantSeeder>();
+await seeder.Seed();
+
+// Configure the HTTP request pipeline.
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
---
+++ Restaurants.Infrastructure/Extension/ServiceCollectionExtension.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Restaurants.Infrastructure.Persistence;
+using Restaurants.Infrastructure.Seeders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Restaurants.Infrastructure.Extension;
+
+public static class ServiceCollectionExtension
+{
+    public static void AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+    {
+        string connectionString = configuration.GetConnectionString("RestaurantDb");
+        services.AddDbContext<RestaurantsDbContext>(option => option.UseSqlServer(connectionString));
+
+        services.AddScoped<IRestaurantSeeder, RestaurantSeeder>();
+    }
+}
